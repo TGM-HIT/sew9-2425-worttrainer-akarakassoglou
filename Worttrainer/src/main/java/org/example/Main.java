601ce8d0a75dbe088c22b67384cf9071237d5500
akarakassoglou <akarakassoglou@student.tgm.ac.at>
2024-10-16 15:19:24 +0200

    Interface erstellt

--- Worttrainer/src/main/java/org/example/Main.java
+++ Worttrainer/src/main/java/org/example/Main.java
@@ -1,13 +1,10 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
-
-    // startet den Frame, um zu testen ob er funktioniert.
-    public void start() {
-        Frame f = new Frame();
     }
 
 }
--- Worttrainer/src/main/java/org/example/Umgang.java
+++ Worttrainer/src/main/java/org/example/Umgang.java
@@ -1,3 +1,13 @@
+package org.example;
+
+/**
+ * Interface Umgang, welches die Klassen Speichern und Laden auswechselbar (zB. JSON, XML, usw.) macht.
+ * @author André Karakassoglou
+ * @version 2024-10-16
+ */
 public interface Umgang {
 
+    Trainer laden(String filename);
+
+    void speichern(String filename);
 }
