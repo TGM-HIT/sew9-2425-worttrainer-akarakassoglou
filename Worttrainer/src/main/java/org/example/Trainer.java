980c0b7f16f6dcc06c16ec891844f488aa79277d
akarakassoglou <akarakassoglou@student.tgm.ac.at>
2024-10-16 17:12:20 +0200

    Trainer Klasse umgsetzt

--- Worttrainer/src/main/java/org/example/Trainer.java
+++ Worttrainer/src/main/java/org/example/Trainer.java
@@ -1,71 +1,152 @@
 package org.example;
 
-public class Trainer implements Umgang {
+import java.util.ArrayList;
+
+
+/**
+ * Klasse die sich um die Eintraege und deren Handhabung kümmert.
+ * @author André Karakassoglou
+ * @version 2022-09-13
+ */
+public class Trainer {
 
 	private int zAbgefragt;
-
 	private int zKorrekt;
-
-	private int zFalsch;
-
-	private Umgang umgang;
-
-	private Eintraege eintraege;
-
 	private Eintrag eintrag;
+	private ArrayList<Eintrag> eintraege;
 
-	public Trainer(Eintraege liste) {
-
+	/**
+	 * Konstruktor
+	 */
+	public Trainer() {
+		this.zAbgefragt = 0;
+		this.zKorrekt = 0;
+		this.eintrag = null;
+		this.eintraege = new ArrayList<Eintrag>();
 	}
 
+	/**
+	 * Konstruktor
+	 * @param eintraege Liste von Eintraegen
+	 * @throws IllegalArgumentException
+	 */
+	public Trainer(ArrayList<Eintrag> eintraege) throws IllegalArgumentException{
+		if(eintraege==null){
+			throw new IllegalArgumentException("Liste muss einen Inhalt haben!");
+		}
+		else{
+			this.eintraege=eintraege;
+		}
+	}
+
+	/**
+	 * Getter zAbgefragt
+	 * @return zAbgefragt
+	 */
 	public int getzAbgefragt() {
-		return 0;
+		return this.zAbgefragt;
 	}
 
+	/**
+	 * Setter zAbgefragt
+	 * @param zAbgefragt neue Anzahl der gemachten Durchgänge
+	 * @throws IllegalArgumentException
+	 */
 	public void setzAbgefragt(int zAbgefragt) {
-
+		if(zAbgefragt<0){
+			throw new IllegalArgumentException("Anzahl der Durchgänge muss positiv sein!");
+		}
+		if(this.zKorrekt>zAbgefragt){
+			throw new IllegalArgumentException("Anzahl der Korrekten Antworten darf nicht größer als die Anzahl der Durchgänge sein!");
+		}
+		else{
+			this.zAbgefragt = zAbgefragt;
+		}
 	}
 
+	/**
+	 * Getter zKorrekt
+	 * @return zKorrekt
+	 */
 	public int getzKorrekt() {
-		return 0;
+		return zKorrekt;
 	}
 
+	/**
+	 * Setter zKorrekt
+	 * @param zKorrekt setzt Anzahl korrekt
+	 * @throws IllegalArgumentException
+	 */
 	public void setzKorrekt(int zKorrekt) {
-
+		if (zKorrekt<0||zKorrekt>this.zAbgefragt){
+			throw new IllegalArgumentException("Anzahl korrekt muss zwischen 0 und Anzahl abgefragt liegen!");
+		}
+		else {
+			this.zKorrekt = zKorrekt;
+		}
 	}
 
-	public Eintraege getEintraege() {
-		return null;
+	/**
+	 * Einen Eintrag hinzufügen
+	 * @param eintrag neuer Eintrag
+	 */
+	public void addEintrag(Eintrag eintrag) {
+		if(eintrag==null){
+			throw new IllegalArgumentException("Der Eintrag darf nicht null sein!");
+		}
+		this.eintraege.add(eintrag);
 	}
 
+	/**
+	 * Einen zufälligen Eintrag aus der Liste zurückgeben
+	 * @return zufälliger Eintrag
+	 */
 	public Eintrag rndmEintrag() {
-		return null;
+		return this.eintraege.get((int) (Math.random() * this.eintraege.size()));
 	}
 
+	/**
+	 * Getter Eintrag
+	 * @return
+	 */
 	public Eintrag getEintrag() {
-		return null;
+		return this.eintrag;
 	}
 
+	/**
+	 * Überprüft ob der mitgegebene Text, dem eines Eintrags entspricht
+	 * @param txt mitgegebenes Wort
+	 * @return ob entspricht oder nicht
+	 */
 	public boolean check(String txt) {
+		++this.zAbgefragt;
+		if (this.eintrag.getWort().equals(txt)) {
+			++this.zKorrekt;
+			return true;
+		}
 		return false;
 	}
 
+
+	/**
+	 * toString Methode
+	 * @return als String
+	 */
 	public String toString() {
-		return null;
+		return "Trainer{" +
+				"zAbgefragt=" + zAbgefragt +
+				", zKorrekt=" + zKorrekt +
+				", eintrag=" + eintrag +
+				", eintraege=" + eintraege +
+				'}';
 	}
 
+	/**
+	 * Methode um die Werte zurückzusetzen
+	 */
 	public void zurueck() {
-
-	}
-
-	@Override
-	public Trainer laden(String filename) {
-		return null;
-	}
-
-	@Override
-	public void speichern(String filename) {
-
+		this.zAbgefragt=0;
+		this.zKorrekt=0;
 	}
 
 }
