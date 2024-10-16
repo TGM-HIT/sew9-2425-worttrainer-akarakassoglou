d9b8f56ccc8833e52ede6813d0aa29e426328241
akarakassoglou <akarakassoglou@student.tgm.ac.at>
2024-10-16 14:20:16 +0200

    Frame erstellt

--- Worttrainer/src/main/java/org/example/Frame.java
+++ Worttrainer/src/main/java/org/example/Frame.java
@@ -1,7 +1,22 @@
-public class Frame {
+package org.example;
 
-	public void Frame(WortPanel layout) {
+import javax.swing.*;
+import java.awt.*;
 
+/**
+ * Frame Klasse des Worttrainers
+ * @author André Karakassoglou
+ * @version 2024-10-16
+ */
+
+public class Frame extends JFrame {
+	public Frame(Panel fensterlayout) {
+		super("WortTrainer von André Karakassoglou");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		super.add(fensterlayout);
+		this.setBounds(0, 0, 900, 750);
+		this.setVisible(true);
 	}
+
 
 }
