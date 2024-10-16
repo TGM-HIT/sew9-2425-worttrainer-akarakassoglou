442d64ba8b909815080a92f80389fa72b60fcfb1
akarakassoglou <akarakassoglou@student.tgm.ac.at>
2024-10-16 15:28:27 +0200

    Eintrag Klasse Konstruktor/Setter/Getter umgesetzt

--- Worttrainer/src/main/java/org/example/Eintrag.java
+++ Worttrainer/src/main/java/org/example/Eintrag.java
@@ -1,27 +1,70 @@
+package org.example;
+
+import java.net.URL;
+
+/**
+ * Klasse Worteintrag erstellt, besteht aus Wort und URL und prüft auf Gültigkeit
+ * @author André Karakassoglou
+ * @version 2024-10-16
+ */
+
 public class Eintrag {
 
 	private String wort;
 
 	private String bild;
 
+
+	/**
+	 * Konstruktor
+	 * @param wort neues Wort
+	 * @param url neue URL
+	 */
 	public Eintrag(String wort, String url) {
-
+		setBild(url);
+		setWort(wort);
 	}
 
+	/**
+	 * Getter wort
+	 * @return das wort
+	 */
 	public String getWort() {
-		return null;
+		return this.wort;
 	}
 
+	/**
+	 * Setter wort
+	 * @param wort neues Wort
+	 */
 	public void setWort(String wort) {
-
+		if((wort!=null)&&(wort.length()>=2)){
+			this.wort = wort;
+		}
+		else{
+			throw new IllegalArgumentException("Es muss ein Wort angegeben werden!");
+		}
 	}
 
+	/**
+	 * Getter Bild
+	 * @return die url/Bild
+	 */
 	public String getBild() {
-		return null;
+		return this.bild;
 	}
 
-	public void setBild(String bild) {
 
+	/**
+	 * Setter Bild
+	 * @param bild neues Bild
+	 */
+	public void setBild(String bild) {
+		if (bild != null) {
+			this.bild = bild;
+		} else {
+			throw new IllegalArgumentException("Es muss eine URL angegeben werden!");
+		}
 	}
 
 	public boolean checkBild(String txt) {
